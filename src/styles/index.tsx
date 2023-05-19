80e25867df69b4f45a441f5cf3b7826b92e85338
Jacob <98672859+Jacob-Clayton@users.noreply.github.com>
2023-05-19 15:50:51 +0100

    tailwind config

---
+++ src/styles/index.tsx
@@ -0,0 +1,28 @@
+const styles = {
+    innerWidth: 'xl:max-w-[1280px] sm:w-[90%] md:w-[80%] w-[95%]',
+    interWidth: '2xl:w-[90%] sm:w-[90%] w-[95%]',
+    articleWidth: 'xl:max-w-[1000px] sm:w-[90%] md:w-[80%] w-[95%]',
+
+    paddings: 'sm:p-16 xs:p-8 px-9 py-12',
+    yPaddings: '2xl:py-20 sm:py-16 xs:py-8 py-12',
+    xPaddings: '2xl:px-0 px-6',
+    topPaddings: 'sm:pt-16 xs:pt-8 pt-12',
+    bottomPaddings: 'sm:pb-16 xs:pb-8 pb-12',
+
+    flexCenter: 'flex justify-center items-center',
+    flexStart: 'flex justify-start items-start',
+    flexEnd: 'flex justify-end',
+    navPadding: 'pt-[98px]',
+
+    headerText: 'font-medium 2xl:text-3xl xl:text-3xl lg:text-2xl md:text-2xl sm:text-2xl text-2xl text-off-white',
+    subheaderText: 'font-semibold 2xl:text-2xl xl:text-2xl md:text-xl sm:text-xl text-lg text-off-white',
+    descriptionText: 'font-regular 2xl:text-xl xl:text-[19px] md:text-lg sm:text-[17px] text-md 2xl:leading-[27px] xl:leading-[26px] lg:leading-[25px] md:leading-[24px] sm:leading-[22px] leading-[21px] text-off-white',
+
+    // hero section
+    heroHeading:
+        'font-bold 2xl:text-[102px] xl:text-[89px] lg:text-[70px] md:text-[65px] sm:text-[55px] text-[43px] 2xl:leading-[100px] xl:leading-[90px] lg:leading-[80px] md:leading-[70px] sm:leading-[60px] leading-[45px]',
+    heroDText:
+        'md:w-[212px] sm:w-[80px] w-[60px] md:h-[108px] sm:h-[48px] h-[38px] md:border-[18px] border-[9px] rounded-r-[50px] border-white sm:mx-2 mx-[6px]',
+};
+
+export default styles;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,20 @@
 import { type Config } from "tailwindcss";
 
 export default {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+    './app/**/*.{html,js,jsx}',
+    './components/**/*.{html,js,jsx}',
+    './sections/**/*.{html,js,jsx}',
+    './styles/**/*.{js,jsx}',
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'off-white' : '#fafafa',
+        'primary-black': '#18181c',
+      }
+    },
   },
   plugins: [],
 } satisfies Config;
