552fa7f6d282d7b36c007e90b9afe04db201eeb6
Jacob <98672859+Jacob-Clayton@users.noreply.github.com>
2023-05-21 19:32:43 +0100

    rough-annotation added to Hero with highlights and circle animation once font has loaded.

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,46 +1,72 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import styles from '../src/styles';
-import { annotate } from 'rough-notation';
+import React from 'react';
+import { useIsFontReady } from 'lib/useIsFontReady';
+import { LIGHT_COLORS } from 'lib/constants';
+import { shuffleArray } from 'lib/shuffleArray';
+import { RainbowHighlight } from 'lib';
+import { RoughNotation, RoughNotationGroup } from "react-rough-notation";
 
 const Hero = () => {
 
+    // Before animation, detect if custom fonts are loaded, so <RoughNotation />
+    // SVG's are correctly positioned over the elements
+    const isFontReady = useIsFontReady()
+
+    const [colors, setColors] = React.useState<string[]>([])
+
+    // Shuffle our colors and store them in state so the order is persisted during
+    // React re-renders
+    React.useEffect(() => {
+        setColors(shuffleArray(LIGHT_COLORS))
+    }, [])
+
     return (
         <section id="Home" className={`${styles.innerWidth} flex mx-auto text-center`}>
-        <div className={`${styles.yPaddings} mt-[120px] flex sm:flex-row flex-col my-auto justify-between sm:gap-0 gap-10`}>      
-        <div className={`md:w-2/3 w-full flex-col items-center self-center z-20`}>
-            <p className={`${styles.heroHeading} sm:text-left text-center 2xl:px-0 sm:px-6 px-0`}>
-                Hello! I'm Jacob, a developer based in England.
-            </p>
-            <div className={`${styles.xPaddings} ${styles.descriptionText} sm:w-full sm:text-left text-center sm:mt-6 md:mt-8 mt-5 sm:mx-[0px] mx-auto space-y-3`}>
-                <p>I love building things that solve problems in a user-friendly way.</p>
-                <p>I am a self-taught developer, starting out in 2020 by building a machine learning algorithm for my Undergraduate dissertation, I never looked back. Now I use Javascript and Python regularly.</p>
-                <p>I'm currently looking for a role as a developer. Hire me?</p>
-            </div>
-            <div className={`${styles.xPaddings} flex flex-row justify-center sm:justify-start sm:gap-6 gap-4 sm:mt-8 md:mt-10 mt-6`}>
-                <button className="border border-primary-black py-2 px-7 items-center flex flex-row rounded bg-primary-black ">
-                <Link className={`${styles.descriptionText} text-slate-200 font-light`} href="https://www.linkedin.com/in/jebclayton/">
-                    View Linkedin</Link>
-                </button>
-                <button className='border border-primary-black py-2 px-7 items-center flex flex-row rounded'>
-                <Link className={`${styles.descriptionText} font-light`} href="https://github.com/Jacob-Clayton">
-                    View Github</Link>
-                </button>
-            </div>
-        </div>
+            <div className={`${styles.yPaddings} mt-[120px] flex sm:flex-row flex-col my-auto justify-between sm:gap-0 gap-10`}>      
+                <div className={`md:w-2/3 w-full flex-col items-center self-center z-20`}>
+                    <RoughNotationGroup show={isFontReady}>
+                    <p className={`${styles.heroHeading} sm:text-left text-center 2xl:px-0 sm:px-6 px-0`}>
+                        Hello! I'm Jacob, a 
+                        <RainbowHighlight color={colors[0]}> developer </RainbowHighlight>
+                        based in England.
+                    </p>
+                    <div className={`${styles.xPaddings} ${styles.descriptionText} sm:w-full sm:text-left text-center sm:mt-6 md:mt-8 mt-5 sm:mx-[0px] mx-auto space-y-3`}>
+                        <p>I love building things that are 
+                            <RainbowHighlight color={colors[1]}> user-friendly, simple </RainbowHighlight> 
+                            and <RainbowHighlight color={colors[2]}>solve problems.</RainbowHighlight>
+                        </p>
+                        <p>I am a self-taught developer, starting out in 2020 by building a machine learning algorithm for my Undergraduate dissertation, I never looked back. Now I use <RainbowHighlight color={colors[3]}>Javascript and Python</RainbowHighlight> regularly.</p>
+                        <p>I'm currently looking for a role as a developer.
+                            <RoughNotation type='circle' color='orange' strokeWidth={2}> Hire me?</RoughNotation>
+                        </p>
+                    </div>
+                    </RoughNotationGroup>
+                    <div className={`${styles.xPaddings} flex flex-row justify-center sm:justify-start sm:gap-6 gap-4 sm:mt-8 md:mt-10 mt-6`}>
+                        <button className="border border-primary-black py-2 px-7 items-center flex flex-row rounded bg-primary-black ">
+                        <Link className={`${styles.descriptionText} text-slate-100 font-light`} href="https://www.linkedin.com/in/jebclayton/">
+                            View Linkedin</Link>
+                        </button>
+                        <button className='border border-primary-black py-2 px-7 items-center flex flex-row rounded'>
+                        <Link className={`${styles.descriptionText} font-light`} href="https://github.com/Jacob-Clayton">
+                            View Github</Link>
+                        </button>
+                    </div>
+                </div>
 
-            <div className='md:block hidden my-auto md:w-1/3 lg:w-1/2'>
-                <Image
-                    src={'/macbookcode.png'}
-                    alt="Profile"
-                    priority={true}
-                    width={1000}
-                    height={1000}
-                />
-            </div>
+                <div className='md:block hidden my-auto md:w-1/3 lg:w-1/2'>
+                    <Image
+                        src={'/macbookcode.png'}
+                        alt="Profile"
+                        priority={true}
+                        width={1000}
+                        height={1000}
+                    />
+                </div>
 
-        </div>
-    </section>
+            </div>
+        </section>
     );
 };
 
