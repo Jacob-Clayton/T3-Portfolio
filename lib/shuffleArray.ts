42238bbe82f5295dc0f87d492cc3c9228b0bf87a
Jacob <98672859+Jacob-Clayton@users.noreply.github.com>
2023-05-23 16:32:50 +0100

    shuffleArray let temp => const temp

--- lib/shuffleArray.ts
+++ lib/shuffleArray.ts
@@ -2,7 +2,7 @@
 export function shuffleArray(array: (string | undefined)[]) {
     for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1))
-    let temp = array[i]
+    const temp = array[i]
     array[i] = array[j]
     array[j] = temp
     }
